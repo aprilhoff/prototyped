c273889e1c537e0e40fff52c414ebc82a1653c33
aprilhoff <hoffa617@newschool.edu>
2025-05-04 14:31:37 -0400

    .

--- test.js
+++ test.js
@@ -2,4 +2,68 @@
 
 
 let book = document.querySelector("#book");
+let postcard = document.querySelector("#postcard");
+let magazine = document.querySelector("#magazine")
 let poster = document.querySelector("#poster");
+let flyer = document.querySelector("#flyer");
+
+book.addEventListener("click", () => {
+    let items = document.querySelectorAll(".archive_grid a");
+
+    items.forEach(item => {
+        if (item.getAttribute("data-type") === "book") {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
+
+postcard.addEventListener("click", () => {
+    let items = document.querySelectorAll(".archive_grid a");
+
+    items.forEach(item => {
+        if (item.getAttribute("data-type") === "postcard") {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
+
+magazine.addEventListener("click", () => {
+    let items = document.querySelectorAll(".archive_grid a");
+
+    items.forEach(item => {
+        if (item.getAttribute("data-type") === "magazine") {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
+
+poster.addEventListener("click", () => {
+    let items = document.querySelectorAll(".archive_grid a");
+
+    items.forEach(item => {
+        if (item.getAttribute("data-type") === "poster") {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
+
+flyer.addEventListener("click", () => {
+    let items = document.querySelectorAll(".archive_grid a");
+
+    items.forEach(item => {
+        if (item.getAttribute("data-type") === "flyer") {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
+
